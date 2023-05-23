f9d857903ff8195e3b4e47c7654a209cd992b086
MalleeFoul <visittheinfidelw.expln.pamphlets@gmail.com>
2023-05-23 20:58:08 +1000

    modified:   Formula/molsketch.rb     Uuuhh I dont know what I'm doing maybe this will work though? I hope? cos I'm fed up with xdrawchem crashing all the time I want an app to draw organic chem that isnt a pain aaaaaaa

--- Formula/molsketch.rb
+++ Formula/molsketch.rb
@@ -2,27 +2,19 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Molsketch < Formula
-  desc "Molsketch is a 2D molecular editing tool. Its goal is to help you draw molecules quick and easily."
+  desc ""
   homepage "https://molsketch.sourceforge.io/"
-  url "https://sourceforge.net/projects/molsketch/files/Molsketch/Molsketch-0.8.0-src.tar.gz/"
-  version_scheme 1
-  version "0.8.0"
-  sha256 "329c787c792dc6a040926770aa0da95ef120bef19440f6e0ab12b05ca8e126e4"
-  license "GPLv2"
+  url "https://github.com/hvennekate/Molsketch/archive/refs/tags/Fluorine_0.8.0.tar.gz"
+  license ""
+  head "https://github.com/hvennekate/Molsketch.git"
 
   depends_on "cmake" => :build
   depends_on "open-babel"
-  depends_on "qt5" => :build
-  depends_on "pkg-config" => :build
-  depends_on "creduce"
+  depends_on "qt5"
 
   def install
-
-    args = [
-      "-DMSK_PREFIX=#{prefix}",
-    ]
     # ENV.deparallelize  # if your formula fails when building in parallel
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", "-DMSK_PREFIX=#{prefix}" *std_cmake_args
     system "cmake", "--build", "build"
     system "make", "install", "build"
   end
