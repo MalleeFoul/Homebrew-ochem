60d3f92b39fcfebfb243268a84ab5bb7b5107795
MalleeFoul <visittheinfidelw.expln.pamphlets@gmail.com>
2023-05-24 21:06:33 +1000

    modified: formula/molsketch.rb I'll be honest, most of this is thinking "huh, there's gotta be a better way to do this," and then having a crisis because I have no fucking clue what anything is meant to mean

--- Formula/molsketch.rb
+++ Formula/molsketch.rb
@@ -2,19 +2,28 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Molsketch < Formula
-  desc ""
-  homepage ""
+  desc "Molsketch is a 2D molecular editing tool. Its goal is to help you draw molecules quick and easily."
+  homepage "https://molsketch.sourceforge.io/"
   url "https://github.com/hvennekate/Molsketch/archive/refs/tags/Fluorine_0.8.0.tar.gz"
   sha256 "37c960de78e061c202bc8b0e882510b409b154cdd06d457353898ccdb8445f86"
-  license ""
+  license "GPL-2.0"
 
   depends_on "cmake" => :build
+  depends_on "open-babel"
+  depends_on "qt@5" => :build
+  depends_on "qtWidgets" => :qt5
+  depends_on "qtGui" => :qt5
+  depends_on "qtPrintSupport" => :qt5
+  depends_on "qtSvg" => :qt5
 
   def install
+
+    ENV[MSK_PREFIX] = "#{prefix}"
+    ENV[QT_CORE] = "#{Formula["qt@5"].lib}/cmake/Qt5Core}"
     # ENV.deparallelize  # if your formula fails when building in parallel
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
-    system "cmake", "--install", "build"
+    system "make", "install"
   end
 
   test do
